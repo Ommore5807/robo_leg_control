4dd49ed9a24dd593fe8a77514dde1e3868dd757f
Ommore5807 <ommore5807@tamu.edu>
2023-07-25 10:48:04 -0500

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ommore5807/moveit_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ommore5807/moveit_ws/devel;/opt/ros/noetic'.split(';') if '/home/ommore5807/moveit_ws/devel;/opt/ros/noetic' != '' else []
