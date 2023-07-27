b2e4f08a7fdf7a167faabae5f4e09e8b9932127b
Ommore5807 <ommore5807@tamu.edu>
2023-07-27 11:21:03 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ommore5807/moveit_ws/src
+# Build directory: /home/ommore5807/moveit_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robo_leg")
---
+++ build/robo_leg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ommore5807/moveit_ws/src/robo_leg
+# Build directory: /home/ommore5807/moveit_ws/build/robo_leg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
